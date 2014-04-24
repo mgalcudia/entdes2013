5fca81c044abcaa705f190ee91b059155005d31e
Santi-INFOR01 <sadoza@gmail.com>
2014-04-24 18:29:32 +0200

    Añadimos etiqueta

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¡Hola Mundo!";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
 
     }
 }
